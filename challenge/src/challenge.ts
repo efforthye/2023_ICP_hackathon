b70fab48d7759c11bd86ef491d115c8f531fd545
Seoho Kim <seoho@Seohos-MacBookPro.local>
2023-10-20 00:28:33 +0900

    token canister generated

--- challenge/src/challenge.ts
+++ challenge/src/challenge.ts
@@ -1,19 +1,99 @@
-// http://localhost:33505/_/dashboard
-// challenge : http://127.0.0.1:4943/?canisterId=bd3sg-teaaa-aaaaa-qaaba-cai&id=bkyz2-fmaaa-aaaaa-qaaaq-cai
+import { text, nat64, Record, Vec, Opt, update, ic, bool, StableBTreeMap, Canister } from 'azle';
 
-import { Canister, query, text, update, Void } from 'azle';
+export const Challenge = Record({
+    id: text, // Unique challenge ID
+    title: text, // Title of the challenge
+    description: text, // Description of the challenge
+    reward: nat64, // Reward for completing the challenge
+    participants: Vec(text), // Participants in the challenge
+    completed: Vec(text), // Users who completed the challenge
+    creator: text, // The creator of the challenge
+});
 
-// This is a global variable that is stored on the heap
-let message = '';
+let challenges = StableBTreeMap(text, Challenge, 0);
+
+function getCaller(): string {
+    const caller = ic.caller().toString();
+    if (caller === null) {
+        throw new Error('Caller is null');
+    }
+    return caller;
+}
 
 export default Canister({
-    // Query calls complete quickly because they do not go through consensus
-    getMessage: query([], text, () => {
-        return message;
+    createChallenge: update([text, text, nat64], text, (title, description, reward) => {
+        const id = Math.random().toString(36).substring(2); // Create a random unique ID
+        const creator = getCaller();
+
+        const newChallenge = {
+            id,
+            title,
+            description,
+            reward,
+            participants: [],
+            completed: [],
+            creator,
+        };
+
+        challenges.insert(id, newChallenge);
+        return id; // Return challenge ID
     }),
-    // Update calls take a few seconds to complete
-    // This is because they persist state changes and go through consensus
-    setMessage: update([text], Void, (newMessage) => {
-        message = newMessage; // This change will be persisted
+
+    joinChallenge: update([text], bool, (challengeId) => {
+        const user = getCaller();
+
+        const challengeOpt = challenges.get(challengeId);
+        if ('None' in challengeOpt) {
+            return false;
+        }
+
+        const challenge = challengeOpt.Some;
+        if (challenge.participants.indexOf(user) === -1) {
+            challenge.participants.push(user);
+            challenges.insert(challengeId, challenge);
+        }
+
+        return true;
+    }),
+
+    completeChallenge: update([text], bool, (challengeId) => {
+        const user = getCaller();
+
+        const challengeOpt = challenges.get(challengeId);
+        if ('None' in challengeOpt) {
+            return false;
+        }
+
+        const challenge = challengeOpt.Some;
+        if (challenge.completed.indexOf(user) === -1) {
+            challenge.completed.push(user);
+            challenges.insert(challengeId, challenge);
+        }
+
+        return true;
+    }),
+
+    rewardParticipants: update([text], bool, (challengeId) => {
+        const user = getCaller();
+
+        const challengeOpt = challenges.get(challengeId);
+        if ('None' in challengeOpt) {
+            return false;
+        }
+
+        const challenge = challengeOpt.Some;
+        if (challenge.creator !== user) {
+            throw new Error('Only challenge creators can reward participants.');
+        }
+
+        // Logic to distribute rewards to participants using token canister's transfer function
+        // For this example, I'll pseudocode it as it will depend on the token canister's API.
+        /*
+        for (let participant of challenge.completed) {
+            tokenCanister.transfer(participant, challenge.reward);
+        }
+        */
+
+        return true;
     }),
 });
