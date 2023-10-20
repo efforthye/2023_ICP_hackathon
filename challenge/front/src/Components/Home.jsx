5c7198ba1da2e484cc96be1a62de5df122ae7bac
efforthye <efforthye@gmail.com>
2023-10-20 10:38:31 +0900

    [FEAT] front structure

---
+++ challenge/front/src/Components/Home.jsx
@@ -0,0 +1,20 @@
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+
+const Home = () => {
+    return (
+        <>
+            <AllWrap>
+                <div>home</div>
+            </AllWrap>
+        </>
+    );
+};
+
+export default Home;
+
+const AllWrap = styled.div`
+    padding: 40px;
+`;
