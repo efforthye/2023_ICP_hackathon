407e681d5dc1265b5bade5dfab9bd392896bfcc6
JongHyunJung <jjh19960115@gmail.com>
2023-10-20 20:56:05 +0900

    [FEAT] create Loading

--- challenge/front/src/App.js
+++ challenge/front/src/App.js
@@ -1,26 +1,55 @@
 import styled from 'styled-components';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, createBrowserRouter } from 'react-router-dom';
 import Header from './Components/Header.jsx';
 import Home from './Components/Home.jsx';
 import Suggest from './Components/Suggest.jsx';
+import { useEffect, useState } from 'react';
+import LoadingScreen from './Components/Loading-screen.jsx';
 
 function App() {
+    // 일단 icp에 요청을 보내서 처음에 user 정보를 cookie에 가지고 있다가
+    // 해당 정보로 계속 다시 요청 보내면 될 것 같다.
+    // https://m.blog.naver.com/PostView.naver?blogId=francisec&logNo=222383873268&categoryNo=1&proxyReferer=
+    // 참고 : https://github.com/therealbryanho/dfinity-websitewithcms
+    // 치트키 : https://dfinityorg.notion.site/ICP-Hackathon-Cheat-Sheet-b2921239266149de81021412f572351c
+    
+    const [isLoading, setLoading] = useState(true);
+    const init = async() => {
+        setLoading(false);
+    };
+    useEffect(() => {
+        init();
+    }, []);
+
     return (
         <AllWrap className="App">
             <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/suggest" element={<Suggest />} />
-            </Routes>
+            {isLoading ? (
+                // 만약 isLoading이 true이면 로딩 화면을 표시합니다.
+                <LoadingScreen />
+            ) : (
+                // 그렇지 않으면 실제 내용을 표시합니다.
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/suggest" element={<Suggest />} />
+                </Routes>
+            )}
         </AllWrap>
     );
 }
 
 export default App;
 
+const Height60Box = styled.div`
+    height: 60px;
+`;
+const Height80Box = styled.div`
+    height: 80px;
+`;
+
 const AllWrap = styled.div`
     width: 1400px;
-    margin: 0 auto;
-    height: 100vh;
-    background-color: white;
+    margin: 10px auto;
+    box-sizing: border-box;
+    min-height: 800px;
 `;
--- challenge/front/src/Components/Suggest.jsx
+++ challenge/front/src/Components/Suggest.jsx
@@ -6,15 +6,191 @@ import styled from 'styled-components';
 const Suggest = () => {
     return (
         <>
-            <AllWrap>
-                <div>suggest</div>
-            </AllWrap>
+            <TopText>Top. 8 Challenge</TopText>
+            <ChallengeAllWrap>
+                {/* 8개 */}
+                {/* {challenges.map((challenge, index) => (
+                <ChallengeBox key={index}>
+                <div>{challenge.title}</div>
+                <div>{challenge.image}</div>
+                <div>{challenge.summary}</div>
+                <div>{challenge.timestamp}</div>
+                <div>참여하기</div>
+                </ChallengeBox>
+                ))} */}
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+            </ChallengeAllWrap>
+
+            <LatestText>Latest Challenge</LatestText>
+            <ChallengeAllWrap>
+                {/* 12개 */}
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+                <ChallengeBox>
+                    <div>제목</div>
+                    <div>이미지</div>
+                    <div>내용 요약</div>
+                    <div>작성 시각</div>
+                    <div>참여하기</div>
+                </ChallengeBox>
+            </ChallengeAllWrap>
         </>
     );
 };
 
 export default Suggest;
 
-const AllWrap = styled.div`
-    padding: 40px;
+const TopText = styled.div`
+    padding: 10px;
+    margin-top: 50px;
+    font-weight: 700;
+    font-size: 20px;
+    /* background: linear-gradient(0deg, #ff3263 25.49%, #6d6aff 92.08%); */
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-color: red;
+`;
+const LatestText = styled.div`
+    padding: 10px;
+    margin-top: 50px;
+    font-weight: 700;
+    font-size: 20px;
+    background: linear-gradient(0deg, #ff3263 25.49%, #6d6aff 92.08%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-color: #1194cd;
+    /* background-color: red; */
+`;
+
+const ChallengeAllWrap = styled.div`
+    border-radius: 10px;
+`;
+
+const ChallengeBox = styled.div`
+    width: 310px;
+    height: 340px;
+    border-radius: 10px;
+    display: inline-block;
+    padding: 10px;
+    margin: 10px;
+    overflow: scroll;
+    background-color: white;
 `;
